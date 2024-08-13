f1199b7b8b4d9a2fb7c3ff3da1078b5beb5c9fc1
Pooja <85984734+pooja170822@users.noreply.github.com>
2024-08-13 20:51:29 +0530

    Add files via upload
    Login-Sign Up Interface in React Js

---
+++ login-signup/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginSignup from './Components/Login-Signup/LoginSignup';
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
